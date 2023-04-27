752b8a28afc2f3b3eed9bd8153e38afa165f8679
Antoine Costes <zgrubul@gmail.com>
2023-04-27 12:33:35 +0200

    fixed battery reading

--- src/sensors/BatteryManager.cpp
+++ src/sensors/BatteryManager.cpp
@@ -3,7 +3,7 @@
 #ifdef ESP32
 BatteryManager::BatteryManager() : Manager("battery")
 {
-    analogSetPinAttenuation(A13, ADC_0db);
+    analogSetPinAttenuation(A13, ADC_11db);
     serialDebug = MASTER_DEBUG;
 }
 
@@ -18,8 +18,8 @@ void BatteryManager::update()
     if (millis() > lastPingMs + BATTERY_TIMOUT_MS)
     {
         analogValue = analogRead(A13);
-        voltage = (2*analogValue / 4096.0f)*3.3f;
-        normValue = voltage / 4.2f;
+        voltage = (2*analogValue / 4095.0f)*3.3f*1.123f;
+        normValue = min(1.0f, max(0.0f, (voltage - 3.5f)/(4.2f - 3.5f))); // [0-1] from 3.5V to 4.2V
 
         sendEvent(BatteryEvent(BatteryEvent::Type::PING, normValue, voltage, analogValue));
         lastPingMs = millis();
