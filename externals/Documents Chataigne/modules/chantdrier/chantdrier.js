9472ba7f7d146d02931fb7a277cde3fa17ac8090
Antoine Costes <zgrubul@gmail.com>
2023-11-13 15:02:15 +0100

    update modules 1.4.8

--- externals/Documents Chataigne/modules/chantdrier/chantdrier.js
+++ externals/Documents Chataigne/modules/chantdrier/chantdrier.js
@@ -10,7 +10,8 @@ function update()
 
 function yo()
 {
-local.parameters.oscOutputs.oscOutput.local.set(false);
+local.values.firmware.set("");
+local.values.ip.set("");
 local.send("/yo", 0);
 local.sendTo("192.168.0.255", 9000, "/yo", 0);
 local.sendTo("192.168.1.255", 9000, "/yo", 0);
@@ -23,12 +24,15 @@ function moduleParameterChanged(param)
 {
   if (param.name == "invocation")
   {
-    local.parameters.ip.set("");
     yo();
   }
-  if (param.name == "play")
+  if (param.name == "playSequence")
   {
     playSequence(local.parameters.sequences.getKey(), 30);
+  }
+  if (param.name == "deleteSequence")
+  {
+    deleteSequence(local.parameters.sequences.getKey(), 30);
   }
 }
 
@@ -40,38 +44,49 @@ function moduleValueChanged(value) {
 function oscEvent(address, args)
 {
 	script.log("OSC Message received "+address+", "+args.length+" arguments");
-  if (args[0] != "Chantdrier") return;
+  if (local.parameters.filterWithName.get() && args[0] != "Chantdrier") return;
 
-  if (address == "/yo")
-  {
-    local.parameters.ip.set(args[1]);
-  }
-
-  if (address == "/battery")
-  {
-      local.values.batterie.set(args[1]);
-  }
-
-  if (address == "/sequences")
-  {
-    local.parameters.sequences.removeOptions();
-    for (var i = 1; i < args.length; i++)
+    if (address == "/yo")
     {
-      local.parameters.sequences.addOption(args[i], i - 1);
+      // NAME, FIRMWARE, MAC, IP, port
+      local.values.firmware.set(args[1]);
+      local.parameters.oscOutputs.oscOutput.local.set(false);
+      local.parameters.oscOutputs.oscOutput.remoteHost.set(args[3]);
+      local.parameters.oscOutputs.oscOutput.remotePort.set(args[4]);
+      local.values.ip.set(args[3]);//+":"+String(args[4]));
     }
-  }
-  if (address == "/tracks")
-  {
-    local.parameters.tracks.removeOptions();
-    for (var i = 1; i < args.length; i++)
-      local.parameters.tracks.addOption(args[i], i - 1);
-  }
+
+    if (address == "/files/sequences")
+    {
+      local.parameters.sequences.removeOptions();
+      for (var i = 1; i < args.length; i++)
+      {
+        local.parameters.sequences.addOption(args[i], i - 1);
+      }
+    }
+
+    if (address == "/musicmaker/tracks")
+    {
+      local.parameters.audioTracks.removeOptions();
+      for (var i = 1; i < args.length; i++)
+        local.parameters.audioTracks.addOption(args[i], i - 1);
+    }
+
+    if (address == "/musicmaker/sd")
+    {
+        local.values.carteSDDetectee.set(args[1]>0);
+    }
+
+    if (address == "/battery")
+    {
+        local.values.batterie.set(args[1]);
+    }
 }
 
 // COMMANDS
-function setServo(index, val) {
-  script.log("Set servo# " + index + ": " + val);
-  local.send("/servo", index, val);
+function setServo(val) {
+  script.log("Set servo " + val);
+  local.send("/servo", 0, val);
 }
 
 function setVolume(val) {
@@ -80,6 +95,10 @@ function setVolume(val) {
 
 function playSequence(name) {
   local.send("/play", name);
+}
+
+function deleteSequence(name) {
+  local.send("/delete", name);
 }
 
 function stopSequence() {
